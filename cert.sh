26b7d1ea23fbd71543d8d4b302c4dea693f6e653
sussa3007 <sussa1933@gmail.com>
2024-08-22 20:14:58 +0900

    Change SSL certificate paths for main SSL domain
    Updated the SSL certificate and key paths to use the main-gj.com domain. This ensures the site's HTTPS configuration is tied to the correct domain for secure connections.

--- cert.sh
+++ cert.sh
@@ -1,9 +1,9 @@
 
 docker-compose run --rm --entrypoint "\
 certbot certonly \
--d main-gj.com \
 -d api.main-gj.com \
 -d admin.main-gj.com \
+-d main-gj.com \
 --email ttea@gmail.com \
 --manual --preferred-challenges dns \
 --server https://acme-v02.api.letsencrypt.org/directory \
